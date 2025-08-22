a503f6a1db0b17090d1393db322e9ad3f907ad85
Roberto <roberto@rober.to>
2025-08-22 15:36:04 +1000

    fix search note

--- itunes.py
+++ itunes.py
@@ -5,7 +5,7 @@ import requests
 st.title("👩🏻‍🎤 Music Previewer 👩🏻‍🎤")
 st.markdown("Search for songs and listen to 30-second previews!")
 
-search_term = st.text_input("Search for a song or artist", "Everything But the Girl")
+search_term = st.text_input("Search for a song or artist, you can type artist song name, ex:", "Everything But the Girl wrong")
 st.markdown("By @soyroberto")
 if st.button("Search"):
     url = f"https://itunes.apple.com/search?term={search_term}&media=music&limit=10"
